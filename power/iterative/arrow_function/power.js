d6760b5a6dd5c75e2937ec5cf63207d99a66bc13
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-07-23 07:59:32 -0500

    Add files via upload

---
+++ power/iterative/arrow_function/power.js
@@ -0,0 +1,10 @@
+power = (n,m) =>{
+ var count = 0;
+  while(count < m-1){
+  n =  n*2;
+   count = count + 1;
+ } 
+  return n;
+}
+power(2,4);
+
---
+++ power/recursive/arrow_function/power.js
@@ -0,0 +1,7 @@
+power = (n,m) => {
+if(m === 0)
+  return 1;
+else
+  return n*power(n,m-1);
+}
+power(2,4);
